fad0a95fd96cd8c7bf239e2059481aa687429393
Talya-Perets <talyape123@gmail.com>
2024-11-26 00:09:52 +0200

    yalaa

---
+++ Assets/Scripts/2-spawners/LaserShooter.cs
@@ -0,0 +1,52 @@
+﻿
+
+using UnityEngine;
+
+/**
+ * This component spawns the given laser-prefab whenever the player clicks a given key.
+ * It also updates the "scoreText" field of the new laser.
+ */
+public class LaserShooter : ClickSpawner
+{
+    [SerializeField]
+    [Tooltip("How many points to add to the shooter, if the laser hits its target")]
+    int pointsToAdd = 1;
+
+    // A reference to the field that holds the score that has to be updated when the laser hits its target.
+    NumberField scoreField;
+
+    private void Start()
+    {
+        // Find the GameObject named "Point" in the scene
+        GameObject pointObject = GameObject.Find("ScoreField");
+
+        // Make sure the object was found
+        if (pointObject != null)
+        {
+            // Get the NumberField component attached to the "Point" GameObject
+            scoreField = pointObject.GetComponent<NumberField>();
+        }
+        else
+        {
+            Debug.LogError("The GameObject 'ScoreField' was not found in the scene!");
+        }
+
+        // Check if the scoreField was successfully assigned
+        if (!scoreField)
+        {
+            Debug.LogError($"No NumberField component found on {pointObject.name}!");
+        }
+    }
+
+    protected override GameObject spawnObject()
+    {
+        GameObject newObject = base.spawnObject();  // base = super
+
+        // Modify the text field of the new object;;.
+        ScoreAdder newObjectScoreAdder = newObject.GetComponent<ScoreAdder>();
+        if (newObjectScoreAdder)
+            newObjectScoreAdder.SetScoreField(scoreField).SetPointsToAdd(pointsToAdd);
+
+        return newObject;
+    }
+}
---
+++ Assets/Scripts/GameOverOnTrigger2D.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverOnTrigger2D : MonoBehaviour
+{
+    [Tooltip("Every object tagged with this tag will trigger game over")]
+    [SerializeField] string triggeringTag;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == triggeringTag && enabled)
+        {
+            Debug.Log("Game over!");
+            Application.Quit(); // עוצר את המשחק במצב Runtime
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; // עוצר את המשחק רק ב-Editor
+#endif
+        }
+    }
+
+    private void Update()
+    {
+        /* Just to show the enabled checkbox in Editor */
+    }
+}
